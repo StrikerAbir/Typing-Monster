e38cd884a468f1fe303d91bad505e3ef9b752e99
Abir Hasan <cse.170201063@gmail.com>
2022-09-15 02:39:19 +0600

    assignment completed with finish. changing text by button done

--- script.js
+++ script.js
@@ -140,3 +140,5 @@ setInterval(() => {
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
+
+document.getElementById("reload").addEventListener("click", () => location.reload())
